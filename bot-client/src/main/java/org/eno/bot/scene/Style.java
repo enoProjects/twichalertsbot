1f8eb27481c0762f72e2e976d5d523b674beab7f
rikkola <toni@rikkola.net>
2021-08-26 22:35:53 +0300

    forgot this one

--- bot-client/src/main/java/org/eno/bot/scene/Style.java
+++ bot-client/src/main/java/org/eno/bot/scene/Style.java
@@ -4,4 +4,14 @@ import org.treblereel.gwt.xml.mapper.api.annotation.XMLMapper;
 
 @XMLMapper
 public class Style {
+
+    private String backgroundColor;
+
+    public String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
 }
